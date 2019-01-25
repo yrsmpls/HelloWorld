6c2f359f6818188ef71d26c14c83ab21584b4a9f
yr <yr.yoghurt@gmail.com>
2019-01-25 15:38:09 +0800

    restore
    restore to prev

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -17,9 +17,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
-        System.out.println("Git Command Test!");
-        System.out.println("Need add again ?");
-        System.out.println("another commit test");
     }
     
 }
